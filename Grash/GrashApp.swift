30a35e9a766e41cdc210ec943f43cf9e8bccc0a4
Jakob Egger <jakob@eggerapps.at>
2023-04-30 14:04:20 +0200

    Add empty Xcode project

---
+++ Grash/GrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrashApp.swift
+//  Grash
+//
+//  Created by Jakob Egger on 29.04.23.
+//
+
+import SwiftUI
+
+@main
+struct GrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
